82037509f2693e30fca2d2e67032337fa0179b03
Lidya046 <lidyaputririal@gmail.com>
2025-04-26 00:34:12 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\ProductController;
 
+// Hanya ini yang dipakai
 Route::get('/list-product', [ProductController::class, 'index']);
-
-Route::get('/list-product', function () {
-    $data = [
-        ['id' => 1, 'produk' => 'Iphone 12'],
-        ['id' => 2, 'produk' => 'Iphone 14'],
-        ['id' => 3, 'produk' => 'Iphone 16'],
-    ];
-    return view('list_product', ['data' => $data]);
-});
