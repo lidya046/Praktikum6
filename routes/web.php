bd9af4ae29e1f80d752790be5fc86a0f9d7841be
Lidya046 <lidyaputririal@gmail.com>
2025-04-26 00:26:40 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\ProductController;
 
 Route::get('/list-product', [ProductController::class, 'index']);
+
+Route::get('/list-product', function () {
+    $data = [
+        ['id' => 1, 'produk' => 'Gitar'],
+        ['id' => 2, 'produk' => 'Biola'],
+        ['id' => 3, 'produk' => 'Drum'],
+    ];
+    return view('list_product', ['data' => $data]);
+});
